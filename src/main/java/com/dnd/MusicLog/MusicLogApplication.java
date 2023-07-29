2b703b9b877d525e605925320f4a79623fb00dc6
hyeonwook98 <hyeonwook98@naver.com>
2023-07-29 22:52:11 +0900

    init: 프로젝트 초기 생성

---
+++ src/main/java/com/dnd/MusicLog/MusicLogApplication.java
@@ -0,0 +1,13 @@
+package com.dnd.MusicLog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicLogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dnd/MusicLog/MusicLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dnd.MusicLog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

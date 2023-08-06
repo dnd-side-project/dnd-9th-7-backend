6f75bfda77282323e5833b3f5cd713b42427dac4
buddy <jang_taehwan@naver.com>
2023-08-06 15:54:40 +0900

    refactor: #2 dto package 위치 수정

--- src/main/java/com/dnd/MusicLog/music/dto/SpotifyTokenResponseDto.java
+++ src/main/java/com/dnd/MusicLog/music/dto/SpotifyTokenResponseDto.java
@@ -1,4 +1,4 @@
-package com.dnd.MusicLog.music.service.dto;
+package com.dnd.MusicLog.music.dto;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;

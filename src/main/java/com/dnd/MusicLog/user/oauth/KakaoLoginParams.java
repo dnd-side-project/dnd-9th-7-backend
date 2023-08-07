8a6b5a108b0c4ac890098eb66d852eb498d42edf
hyeonwook98 <hyeonwook98@naver.com>
2023-08-07 18:14:31 +0900

    feat: #3 OAuth 요청을 위한 인터페이스 및 구현체 개발

---
+++ src/main/java/com/dnd/MusicLog/user/oauth/KakaoLoginParams.java
@@ -0,0 +1,24 @@
+package com.dnd.MusicLog.user.oauth;
+
+import com.dnd.MusicLog.user.enums.OAuthType;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+@Getter
+@NoArgsConstructor
+public class KakaoLoginParams implements OAuthLoginParams {
+    private String authorizationCode;
+    @Override
+    public OAuthType oAuthType() {
+        return OAuthType.KAKAO;
+    }
+
+    @Override
+    public MultiValueMap<String, String> makeBody() {
+        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+        body.add("code", authorizationCode);
+        return body;
+    }
+}

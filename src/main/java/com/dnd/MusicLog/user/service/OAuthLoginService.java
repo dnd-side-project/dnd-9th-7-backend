c184a7d5c82b86347d32646cd52eb45cc7f27eea
hyeonwook98 <hyeonwook98@naver.com>
2023-08-07 18:29:58 +0900

    feat: #3 OAuthLogin 서비스 로직 개발

---
+++ src/main/java/com/dnd/MusicLog/user/service/OAuthLoginService.java
@@ -0,0 +1,52 @@
+package com.dnd.MusicLog.user.service;
+
+import com.dnd.MusicLog.global.jwt.AuthTokensGenerator;
+import com.dnd.MusicLog.user.dto.AuthTokensResponseDto;
+import com.dnd.MusicLog.user.oauth.OAuthInfoResponse;
+import com.dnd.MusicLog.user.oauth.OAuthLoginParams;
+import com.dnd.MusicLog.user.entity.User;
+import com.dnd.MusicLog.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class OAuthLoginService {
+
+    private final UserRepository userRepository;
+    private final AuthTokensGenerator authTokensGenerator;
+    private final RequestOAuthInfoService requestOAuthInfoService;
+
+    @Transactional
+    public AuthTokensResponseDto loginUser(OAuthLoginParams params) {
+        OAuthInfoResponse oAuthInfoResponse = requestOAuthInfoService.request(params);
+        Long userId = validateUserByEmail(oAuthInfoResponse);
+        return authTokensGenerator.generate(userId);
+    }
+
+    private Long validateUserByEmail(OAuthInfoResponse oAuthInfoResponse) {
+        Optional<User> userOptional = userRepository.findByEmail(oAuthInfoResponse.getEmail());
+
+        if (userOptional.isPresent()) {
+            return userOptional.get().getId();
+        } else {
+            return saveUser(oAuthInfoResponse);
+        }
+
+    }
+
+    private Long saveUser(OAuthInfoResponse oAuthInfoResponse) {
+        User user = User.builder()
+            .oauthId(oAuthInfoResponse.getOAuthId())
+            .email(oAuthInfoResponse.getEmail())
+            .nickname(oAuthInfoResponse.getNickname())
+            .profileUrl(oAuthInfoResponse.getProfileUrl())
+            .oauthType(oAuthInfoResponse.getOAuthType())
+            .build();
+
+        return userRepository.save(user).getId();
+    }
+}

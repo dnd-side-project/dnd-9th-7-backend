55a2f22a210bb7486b84e34a11843758f5c8cf0b
hyeonwook98 <hyeonwook98@naver.com>
2023-08-07 17:31:04 +0900

    feat: #3 User Entity 생성

---
+++ src/main/java/com/dnd/MusicLog/user/entity/User.java
@@ -0,0 +1,40 @@
+package com.dnd.MusicLog.user.entity;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "oauth_id", nullable = false)
+    private Long oauth_id;
+
+    private String email;
+
+    @Column(name = "nickname", nullable = false)
+    private String nickname;
+
+    @Column(name = "profile_url", nullable = false)
+    private String profile_url;
+
+    @Column(name = "oauth_type", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private OAuthType oauth_type;
+
+    @Builder
+    public User(Long oauth_id, String email, String nickname, String profile_url, OAuthType oauth_type) {
+        this.oauth_id = oauth_id;
+        this.email = email;
+        this.nickname = nickname;
+        this.profile_url = profile_url;
+        this.oauth_type = oauth_type;
+    }
+}

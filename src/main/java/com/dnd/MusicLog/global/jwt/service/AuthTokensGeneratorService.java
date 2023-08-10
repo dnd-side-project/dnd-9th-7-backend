f976f236865949b1a6641d2eac7e9a37e696481a
hyeonwook98 <hyeonwook98@naver.com>
2023-08-10 14:38:20 +0900

    refactor: #9 메서드명 수정

--- src/main/java/com/dnd/MusicLog/global/jwt/service/AuthTokensGeneratorService.java
+++ src/main/java/com/dnd/MusicLog/global/jwt/service/AuthTokensGeneratorService.java
@@ -1,13 +1,12 @@
 package com.dnd.MusicLog.global.jwt.service;
 
+import com.dnd.MusicLog.global.jwt.util.JwtProperties;
 import com.dnd.MusicLog.global.jwt.util.JwtTokenProvider;
 import com.dnd.MusicLog.global.jwt.dto.AuthTokensResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
-
-import static com.dnd.MusicLog.global.jwt.util.JwtProperties.*;
 
 @Component
 @RequiredArgsConstructor
@@ -16,18 +15,17 @@ public class AuthTokensGeneratorService {
     private final JwtTokenProvider jwtTokenProvider;
 
     public AuthTokensResponseDto generateAuthToken(Long userId) {
+
         long now = (new Date()).getTime();
-        Date accessTokenExpiredAt = new Date(now + ACCESS_TOKEN_EXPIRE_TIME);
-        Date refreshTokenExpiredAt = new Date(now + REFRESH_TOKEN_EXPIRE_TIME);
+        Date accessTokenExpiredAt = new Date(now + JwtProperties.ACCESS_TOKEN_EXPIRE_TIME);
+        Date refreshTokenExpiredAt = new Date(now + JwtProperties.REFRESH_TOKEN_EXPIRE_TIME);
 
         String subject = userId.toString();
-        String accessToken = jwtTokenProvider.generate(subject, accessTokenExpiredAt, ACCESS_TOKEN_TYPE);
-        String refreshToken = jwtTokenProvider.generate(subject, refreshTokenExpiredAt, REFRESH_TOKEN_TYPE);
+        String accessToken = jwtTokenProvider.generateToken(subject, accessTokenExpiredAt, JwtProperties.ACCESS_TOKEN_TYPE);
+        String refreshToken = jwtTokenProvider.generateToken(subject, refreshTokenExpiredAt, JwtProperties.REFRESH_TOKEN_TYPE);
 
-        return AuthTokensResponseDto.of(accessToken, refreshToken, BEARER_TYPE, ACCESS_TOKEN_EXPIRE_TIME / 1000L);
+        return AuthTokensResponseDto.of(accessToken, refreshToken, JwtProperties.BEARER_TYPE,
+            JwtProperties.ACCESS_TOKEN_EXPIRE_TIME / 1000L);
     }
 
-//    public String extractTokenType(String token, String tokenType) {
-//        return String.valueOf(jwtTokenProvider.extractTokenType(token, tokenType));
-//    }
 }

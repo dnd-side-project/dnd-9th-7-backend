478bc00f52f4a75a155b60bcfcb69f03412261f7
hyeonwook98 <hyeonwook98@naver.com>
2023-08-11 18:23:58 +0900

    refactor: #9 어노테이션 수정

--- src/main/java/com/dnd/MusicLog/global/jwt/dto/AuthTokensResponseDto.java
+++ src/main/java/com/dnd/MusicLog/global/jwt/dto/AuthTokensResponseDto.java
@@ -2,17 +2,7 @@ package com.dnd.MusicLog.global.jwt.dto;
 
 import lombok.*;
 
-@Getter
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
-public class AuthTokensResponseDto {
+@Builder
+public record AuthTokensResponseDto(String accessToken, String refreshToken, String grantType, Long expiresIn) {
 
-    private String accessToken;
-    private String refreshToken;
-    private String grantType;
-    private Long expiresIn;
-
-    public static AuthTokensResponseDto of(String accessToken, String refreshToken, String grantType, Long expiresIn) {
-        return new AuthTokensResponseDto(accessToken, refreshToken, grantType, expiresIn);
-    }
 }

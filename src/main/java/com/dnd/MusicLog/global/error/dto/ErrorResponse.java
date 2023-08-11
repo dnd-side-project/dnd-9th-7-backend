cc2c176224b923d1cd94bd7c21161d56044d6bae
hyeonwook98 <hyeonwook98@naver.com>
2023-08-11 15:44:54 +0900

    refactor: #9 불필요한 import 제거

--- src/main/java/com/dnd/MusicLog/global/error/dto/ErrorResponse.java
+++ src/main/java/com/dnd/MusicLog/global/error/dto/ErrorResponse.java
@@ -1,9 +1,6 @@
 package com.dnd.MusicLog.global.error.dto;
 
-
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
 
 @Builder
 public record ErrorResponse(int status, String message) {

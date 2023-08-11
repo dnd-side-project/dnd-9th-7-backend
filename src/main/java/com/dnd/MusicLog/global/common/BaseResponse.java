4de7fe3288e8319ba6836c53785ebb7529fa60bc
hyeonwook98 <hyeonwook98@naver.com>
2023-08-11 17:26:25 +0900

    refactor: #9 BaseResponse 타입 수정

--- src/main/java/com/dnd/MusicLog/global/common/BaseResponse.java
+++ src/main/java/com/dnd/MusicLog/global/common/BaseResponse.java
@@ -1,8 +1,17 @@
 package com.dnd.MusicLog.global.common;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 @Builder
-public record BaseResponse<T>(int status, String message, T data) {
+public class BaseResponse<T> {
+
+    private int status;
+    private String message;
+    private T data;
 
 }

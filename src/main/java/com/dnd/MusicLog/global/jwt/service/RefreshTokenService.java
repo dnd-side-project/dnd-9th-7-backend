2173f8eee65d8c900c3b156bebbc51a8ad7a1f9d
hyeonwook98 <hyeonwook98@naver.com>
2023-08-11 14:23:46 +0900

    refactor: #9 AccessToken 재발급 서비스 수정

--- src/main/java/com/dnd/MusicLog/global/jwt/service/RefreshTokenService.java
+++ src/main/java/com/dnd/MusicLog/global/jwt/service/RefreshTokenService.java
@@ -1,16 +1,16 @@
 package com.dnd.MusicLog.global.jwt.service;
 
+import com.dnd.MusicLog.global.error.exception.BusinessLogicException;
+import com.dnd.MusicLog.global.error.exception.ErrorCode;
 import com.dnd.MusicLog.global.jwt.dto.AccessTokenResponseDto;
 import com.dnd.MusicLog.global.jwt.util.JwtProperties;
 import com.dnd.MusicLog.global.jwt.util.JwtTokenProvider;
-import com.dnd.MusicLog.user.entity.User;
 import com.dnd.MusicLog.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
-import java.util.Optional;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -22,16 +22,13 @@ public class RefreshTokenService {
 
     public AccessTokenResponseDto regenerateAccessToken(String refreshToken, String tokenType) {
 
-        String accessToken = null;
         long now = (new Date()).getTime();
         Date accessTokenExpiredAt = new Date(now + JwtProperties.ACCESS_TOKEN_EXPIRE_TIME);
         String userId = jwtTokenProvider.extractSubject(refreshToken, tokenType);
-        Optional<User> optionalUser = userRepository.findById(Long.valueOf(userId));
-        if (optionalUser.isEmpty()) {
-            log.error("유저 리소스가 없습니다.");
-        } else {
-            accessToken = jwtTokenProvider.generateToken(userId, accessTokenExpiredAt, JwtProperties.ACCESS_TOKEN_TYPE);
-        }
+        userRepository.findById(Long.valueOf(userId))
+            .orElseThrow(() -> new BusinessLogicException(ErrorCode.NOT_FOUND));
+
+        String accessToken = jwtTokenProvider.generateToken(userId, accessTokenExpiredAt, JwtProperties.ACCESS_TOKEN_TYPE);
 
         return AccessTokenResponseDto.of(accessToken);
 

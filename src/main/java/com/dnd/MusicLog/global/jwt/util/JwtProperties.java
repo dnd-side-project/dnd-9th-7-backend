f69da7582562d0b85591feb6b72d9ddf10bb9e0e
hyeonwook98 <hyeonwook98@naver.com>
2023-08-11 17:10:33 +0900

    refactor: #9 JwtProperties 클래스로 변경

--- src/main/java/com/dnd/MusicLog/global/jwt/util/JwtProperties.java
+++ src/main/java/com/dnd/MusicLog/global/jwt/util/JwtProperties.java
@@ -1,12 +1,18 @@
 package com.dnd.MusicLog.global.jwt.util;
 
-public interface JwtProperties {
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
 
-    String BEARER_TYPE = "Bearer";
-    String ACCESS_TOKEN_TYPE = "access";
-    String REFRESH_TOKEN_TYPE = "refresh";
-    String HEADER = "Authorization";
-    long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 30;            // 30분
-    long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 7;  // 7일
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@Component
+public final class JwtProperties {
+
+    public static final String BEARER_TYPE = "Bearer";
+    public static final String ACCESS_TOKEN_TYPE = "access";
+    public static final String REFRESH_TOKEN_TYPE = "refresh";
+    public static final String HEADER = "Authorization";
+    public static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 30;            // 30분
+    public static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 7;  // 7일
 
 }

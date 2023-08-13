cb6e52640f7386ad14dcaced9d8c55e08552281e
hyeonwook98 <hyeonwook98@naver.com>
2023-08-13 20:20:12 +0900

    feat: 토큰 검증 로직 추가

--- src/main/java/com/dnd/MusicLog/log/controller/LogController.java
+++ src/main/java/com/dnd/MusicLog/log/controller/LogController.java
@@ -2,6 +2,7 @@ package com.dnd.MusicLog.log.controller;
 
 import com.dnd.MusicLog.global.common.BaseController;
 import com.dnd.MusicLog.global.common.BaseResponse;
+import com.dnd.MusicLog.global.jwt.util.JwtTokenProvider;
 import com.dnd.MusicLog.log.dto.YoutubeVideoListResponseDto;
 import com.dnd.MusicLog.log.service.SearchYoutubeVideosService;
 import lombok.RequiredArgsConstructor;
@@ -13,12 +14,15 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class LogController extends BaseController {
 
+    private final JwtTokenProvider jwtTokenProvider;
     private final SearchYoutubeVideosService searchYoutubeVideosService;
 
     @GetMapping("/api/youtube")
-    public ResponseEntity<BaseResponse<YoutubeVideoListResponseDto>> searchVideos(@RequestParam String query,
+    public ResponseEntity<BaseResponse<YoutubeVideoListResponseDto>> searchVideos(@RequestHeader(name = "Authorization") String bearerToken,
+                                                                                  @RequestParam String query,
                                                                                   @RequestParam(required = false) String pageToken) {
 
+        jwtTokenProvider.extractAccessTokenSubject(bearerToken);
         YoutubeVideoListResponseDto responseDto = searchYoutubeVideosService.searchYoutubeVideos(query, pageToken);
         return createResponseEntity(HttpStatus.OK, "유튜브 영상 조회 완료", responseDto);
 

f9bd4bbf0b88bad02aa0dadb9cc7968375e6dd3d
hyeonwook98 <hyeonwook98@naver.com>
2023-08-14 17:51:48 +0900

    feat: #14 이미지 업로드 서비스 로직 개발

---
+++ src/main/java/com/dnd/MusicLog/log/service/ImagesUploadService.java
@@ -0,0 +1,61 @@
+package com.dnd.MusicLog.log.service;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.dnd.MusicLog.global.error.exception.BusinessLogicException;
+import com.dnd.MusicLog.global.error.exception.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class ImagesUploadService {
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    private final AmazonS3 amazonS3;
+
+    public List<String> uploadImages(List<MultipartFile> multipartFile, String dirName) {
+
+        if (multipartFile.size() > 10) {
+            throw new BusinessLogicException(ErrorCode.BAD_REQUEST_MULTIPART);
+        }
+
+        List<String> fileNameList = new ArrayList<>();
+
+        multipartFile.forEach(file -> {
+            String fileName = createFileName(file.getOriginalFilename(), dirName);
+            ObjectMetadata objectMetadata = new ObjectMetadata();
+            objectMetadata.setContentLength(file.getSize());
+            objectMetadata.setContentType(file.getContentType());
+
+            try(InputStream inputStream = file.getInputStream()) {
+                amazonS3.putObject(new PutObjectRequest(bucket, fileName, inputStream, objectMetadata)
+                    .withCannedAcl(CannedAccessControlList.PublicRead));
+            } catch(IOException e) {
+                throw new BusinessLogicException(ErrorCode.SERVER_ERROR);
+            }
+
+            fileNameList.add(fileName);
+        });
+
+        return fileNameList;
+    }
+
+    private String createFileName(String fileName, String dirName) {
+        return dirName + "/" + UUID.randomUUID() + fileName;
+    }
+
+}
+

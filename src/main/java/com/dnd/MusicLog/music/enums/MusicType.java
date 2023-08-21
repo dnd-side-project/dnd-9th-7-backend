3b63059c9568c01a047135f28d29b6667d4759ad
hyeonwook98 <hyeonwook98@naver.com>
2023-08-22 01:09:32 +0900

    feat: #31 음악 구분을 위한 enum 타입 추가

---
+++ src/main/java/com/dnd/MusicLog/music/enums/MusicType.java
@@ -0,0 +1,11 @@
+package com.dnd.MusicLog.music.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum MusicType {
+    CUSTOM,
+    SPOTIFY;
+}

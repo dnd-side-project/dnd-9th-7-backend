b94bf9d0e121c408bc7c1fe8234ec519ee233ecb
hyeonwook98 <hyeonwook98@naver.com>
2023-08-21 23:51:11 +0900

    feat: #31 로그 엔티티 생성

---
+++ src/main/java/com/dnd/MusicLog/log/entity/Log.java
@@ -0,0 +1,44 @@
+package com.dnd.MusicLog.log.entity;
+
+import com.dnd.MusicLog.global.common.BaseTimeEntity;
+import com.dnd.MusicLog.music.entity.spotify.SpotifyMusic;
+import com.dnd.MusicLog.user.entity.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Log extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "music_id")
+    private SpotifyMusic spotifyMusic;
+
+    private String location;
+
+    private String record;
+
+    private String review;
+
+    private String youtubeId;
+
+    @Column(name = "temp", nullable = false)
+    private Boolean temp;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "date", nullable = false)
+    private Date date;
+
+}

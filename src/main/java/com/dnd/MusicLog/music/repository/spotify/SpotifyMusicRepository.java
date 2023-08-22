27e972c6704085b55b1e2343b834f676702e4d88
buddy <jang_taehwan@naver.com>
2023-08-22 23:15:57 +0900

    feat: #29 Spotify 음악 단일 검색 (in DB) 로직 작성
    - Spotify 음악 단일 검색(API) 시 DB에 저장하는 로직 구성 필요
    - 저장된 객체들로 검색과 같은 응답을 제공할 수 있도록 핸들링 필요

--- src/main/java/com/dnd/MusicLog/music/repository/spotify/SpotifyMusicRepository.java
+++ src/main/java/com/dnd/MusicLog/music/repository/spotify/SpotifyMusicRepository.java
@@ -1,8 +1,10 @@
 package com.dnd.MusicLog.music.repository.spotify;
 
 import com.dnd.MusicLog.music.entity.spotify.SpotifyMusic;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SpotifyMusicRepository extends JpaRepository<SpotifyMusic, Long> {
 
+    Optional<SpotifyMusic> findBySpotifyId(String spotifyId);
 }
--- src/main/java/com/dnd/MusicLog/music/service/SpotifyMusicService.java
+++ src/main/java/com/dnd/MusicLog/music/service/SpotifyMusicService.java
@@ -1,13 +1,27 @@
 package com.dnd.MusicLog.music.service;
 
-import com.dnd.MusicLog.music.dto.SpotifyItem;
+import com.dnd.MusicLog.music.dto.SpotifyAlbumResponse;
+import com.dnd.MusicLog.music.dto.SpotifyArtistResponse;
+import com.dnd.MusicLog.music.dto.SpotifyImageResponse;
+import com.dnd.MusicLog.music.dto.SpotifyItemResponse;
 import com.dnd.MusicLog.music.dto.SpotifyTokenResponseDto;
 import com.dnd.MusicLog.music.dto.SpotifyTrackResponseDto;
+import com.dnd.MusicLog.music.entity.spotify.SpotifyAlbum;
+import com.dnd.MusicLog.music.entity.spotify.SpotifyArtist;
+import com.dnd.MusicLog.music.entity.spotify.SpotifyMusic;
+import com.dnd.MusicLog.music.entity.spotify.SpotifyMusicArtistRelation;
+import com.dnd.MusicLog.music.repository.spotify.SpotifyAlbumRepository;
+import com.dnd.MusicLog.music.repository.spotify.SpotifyArtistRepository;
+import com.dnd.MusicLog.music.repository.spotify.SpotifyMusicArtistRelationRepository;
+import com.dnd.MusicLog.music.repository.spotify.SpotifyMusicRepository;
+import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -20,6 +34,11 @@ public class SpotifyMusicService {
     private static final String SEARCH_TRACK_REQUEST_URL = "https://api.spotify.com/v1/tracks/";
 
     private final WebClient webClient;
+
+    private final SpotifyMusicRepository musicRepository;
+    private final SpotifyAlbumRepository albumRepository;
+    private final SpotifyArtistRepository artistRepository;
+    private final SpotifyMusicArtistRelationRepository relationRepository;
 
     @Value("${spotify.client.id}")
     private String SPOTIFY_CLIENT_ID;
@@ -40,7 +59,45 @@ public class SpotifyMusicService {
             .block();
     }
 
-    public SpotifyItem searchSpotifyTrack(String spotifyId) {
+    @Transactional
+    public SpotifyItemResponse searchSpotifyTrack(String spotifyId) {
+        Optional<SpotifyMusic> spotifyMusicOptional = musicRepository.findBySpotifyId(spotifyId);
+
+        if (spotifyMusicOptional.isPresent()) {
+            return searchSpotifyTrackInRepository(spotifyMusicOptional.get());
+        }
+
+        // TODO: 반환하기 전 repository 에 저장하는 로직 구성 필요
+        return searchSpotifyTrackByAPI(spotifyId);
+    }
+
+    private SpotifyItemResponse searchSpotifyTrackInRepository(SpotifyMusic spotifyMusic) {
+        SpotifyAlbum album = spotifyMusic.getAlbum();
+        SpotifyImageResponse albumImage = new SpotifyImageResponse(album.getImageUrl(), 0, 0);
+        SpotifyAlbumResponse spotifyAlbumResponse =
+            new SpotifyAlbumResponse(
+                album.getSpotifyId(),
+                album.getName(),
+                0,
+                album.getReleaseDate(),
+                "",
+                List.of(albumImage));
+
+        List<SpotifyMusicArtistRelation> relations = relationRepository.findAllByMusic(spotifyMusic);
+        List<SpotifyArtistResponse> spotifyArtistResponses = relations.stream().map(relation -> {
+            SpotifyArtist artist = relation.getArtist();
+            SpotifyImageResponse image = new SpotifyImageResponse(artist.getImageUrl(), 0, 0);
+            return new SpotifyArtistResponse(artist.getSpotifyId(), artist.getName(), List.of(image));
+        }).toList();
+
+        return new SpotifyItemResponse(
+            spotifyMusic.getSpotifyId(),
+            spotifyMusic.getName(),
+            spotifyAlbumResponse,
+            spotifyArtistResponses);
+    }
+
+    private SpotifyItemResponse searchSpotifyTrackByAPI(String spotifyId) {
         SpotifyTokenResponseDto spotifyTokenResponseDto = getToken();
         String accessToken = spotifyTokenResponseDto.accessToken();
 
@@ -50,7 +107,7 @@ public class SpotifyMusicService {
             .uri(SEARCH_TRACK_REQUEST_URL + queryString)
             .header("Authorization", "Bearer " + accessToken)
             .retrieve()
-            .bodyToMono(SpotifyItem.class)
+            .bodyToMono(SpotifyItemResponse.class)
             .block();
     }
 

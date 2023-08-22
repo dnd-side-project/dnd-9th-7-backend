9a48e52f5cd6f5b6c43d6614f8554fc7d50b0205
hyeonwook98 <hyeonwook98@naver.com>
2023-08-23 00:38:37 +0900

    refactor: #31 연관관계 수정

--- src/main/java/com/dnd/MusicLog/imageinfo/entity/ImageInfo.java
+++ src/main/java/com/dnd/MusicLog/imageinfo/entity/ImageInfo.java
@@ -1,6 +1,7 @@
 package com.dnd.MusicLog.imageinfo.entity;
 
 import com.dnd.MusicLog.global.common.BaseTimeEntity;
+import com.dnd.MusicLog.log.entity.Log;
 import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
@@ -16,16 +17,16 @@ public class ImageInfo extends BaseTimeEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    // TODO : 로그 엔티티 생성 후 연관관계 설정할 것.
+    @ManyToOne(fetch = FetchType.LAZY)
     @Column(name = "log_id", nullable = false)
-    private long logId;
+    private Log log;
 
     @Column(name = "image_name", nullable = false)
     private String imageName;
 
     @Builder
-    public ImageInfo(long logId, String imageName) {
-        this.logId = logId;
+    public ImageInfo(Log log, String imageName) {
+        this.log = log;
         this.imageName = imageName;
     }
 }

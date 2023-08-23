e2587fbd0e1ded5a3bbeae3095e0b38fccb6e709
hyeonwook98 <hyeonwook98@naver.com>
2023-08-23 20:02:50 +0900

    refactor: #35 레포지토리 어노테이션 추가

--- src/main/java/com/dnd/MusicLog/youtubeinfo/repository/YoutubeInfoRepository.java
+++ src/main/java/com/dnd/MusicLog/youtubeinfo/repository/YoutubeInfoRepository.java
@@ -2,6 +2,10 @@ package com.dnd.MusicLog.youtubeinfo.repository;
 
 import com.dnd.MusicLog.youtubeinfo.entity.YoutubeInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+
+@Repository
 public interface YoutubeInfoRepository extends JpaRepository<YoutubeInfo, Long> {
+
 }

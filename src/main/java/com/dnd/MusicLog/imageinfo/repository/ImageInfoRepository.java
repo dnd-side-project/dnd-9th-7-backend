de92f152d05880ec706f6a0498a1df38e46b6ea2
hyeonwook98 <hyeonwook98@naver.com>
2023-08-23 18:10:46 +0900

    refactor: #35 @Param 어노테이션 추가

--- src/main/java/com/dnd/MusicLog/imageinfo/repository/ImageInfoRepository.java
+++ src/main/java/com/dnd/MusicLog/imageinfo/repository/ImageInfoRepository.java
@@ -3,6 +3,7 @@ package com.dnd.MusicLog.imageinfo.repository;
 import com.dnd.MusicLog.imageinfo.entity.ImageInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,7 +11,7 @@ import java.util.Optional;
 public interface ImageInfoRepository extends JpaRepository<ImageInfo, Long> {
 
     @Query("SELECT i.imageName FROM ImageInfo i WHERE i.log.id = :logId ORDER BY i.createdDate ASC")
-    List<String> findAllByLogIdOrderByCreatedDateAsc(long logId);
+    List<String> findAllByLogIdOrderByCreatedDateAsc(@Param("logId") long logId);
 
     Optional<ImageInfo> findByLogId(long logId);
 

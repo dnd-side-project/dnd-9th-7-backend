d0124e0655511cca2a4b53713eb3bc894ede0e3e
hyeonwook98 <hyeonwook98@naver.com>
2023-09-02 22:07:55 +0900

    refactor: #35 로그 전체 정보 응답 dto 수정

--- src/main/java/com/dnd/MusicLog/log/dto/GetFullLogResponseDto.java
+++ src/main/java/com/dnd/MusicLog/log/dto/GetFullLogResponseDto.java
@@ -1,10 +1,5 @@
 package com.dnd.MusicLog.log.dto;
 
-import java.time.LocalDate;
-import java.util.List;
-
-public record GetFullLogResponseDto(String albumImageUrl, List<String> artists, String name, LocalDate date,
-                                    String feeling, String time, String weather, String location, String review,
-                                    String record, List<String> fileUrls, String title, String channelTitle,
-                                    String publishedAt, String youtubeId) {
+public record GetFullLogResponseDto(GetLogMusicResponseDto musicResponseDto, GetLogRecordResponseDto recordResponseDto,
+                                    GetLogPlayResponseDto playResponseDto) {
 }

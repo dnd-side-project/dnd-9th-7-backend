ed1d79112b796c949566b5c3a80469982fa5d1b6
hyeonwook98 <hyeonwook98@naver.com>
2023-09-05 23:58:20 +0900

    refactor: #39 파라미터명 수정

--- src/main/java/com/dnd/MusicLog/log/dto/GetLogMusicResponseDto.java
+++ src/main/java/com/dnd/MusicLog/log/dto/GetLogMusicResponseDto.java
@@ -3,7 +3,7 @@ package com.dnd.MusicLog.log.dto;
 import java.time.LocalDate;
 import java.util.List;
 
-public record GetLogMusicResponseDto(String albumImageUrl, List<String> artists, String songTitle, String review, String feeling, String time,
+public record GetLogMusicResponseDto(String albumImageUrl, List<String> artists, String name, String review, String feeling, String time,
                                      String weather, String location, LocalDate date) {
 
 }

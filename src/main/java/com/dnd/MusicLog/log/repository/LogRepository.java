7a1252ec2b8ce4d800c61afdad10082901c18f54
hyeonwook98 <hyeonwook98@naver.com>
2023-10-05 00:49:19 +0900

    feat: #37 일별 캘린더 정보 조회 레포지토리 메서드 선언

--- src/main/java/com/dnd/MusicLog/log/repository/LogRepository.java
+++ src/main/java/com/dnd/MusicLog/log/repository/LogRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,4 +20,15 @@ public interface LogRepository extends JpaRepository<Log, Long> {
     @Query("SELECT l FROM Log l WHERE l.user.id = :userId AND l.temp = true ORDER BY l.lastModifiedDate DESC")
     List<Log> findByUserIdAndTemp(@Param("userId") long userId);
 
+    // 월별 캘린더 정보 제공 (해당 년도, 월, 앨범 커버 + 일, 기록한 날짜 개수, 기록 개수)
+//    @Query("SELECT l " +
+//        "FROM Log l " +
+//        "WHERE l.user.id = :userId AND l.temp = false " +
+//        "GROUP BY l.date ORDER BY l.lastModifiedDate DESC")
+//    List<Log> findAllByUserIdAndDate(@Param("userId") long userId, String year, String month);
+
+    // 일별 캘린더 정보 제공
+    @Query("SELECT l FROM Log l WHERE l.user.id = :userId AND YEAR(l.date) = YEAR(:date) AND MONTH(l.date) = MONTH(:date) " +
+        "AND DAY(l.date) = DAY(:date) AND l.temp = false ORDER BY l.representation DESC ,l.lastModifiedDate DESC")
+    List<Log> findAllByUserIdAndDay(@Param("userId") long userId, @Param("date") LocalDate date);
 }

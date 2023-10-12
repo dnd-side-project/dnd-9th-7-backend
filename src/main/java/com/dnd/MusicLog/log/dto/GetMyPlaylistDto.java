f9c823fdb0df9422d7a17fbfee36d668a0069ceb
hyeonwook98 <hyeonwook98@naver.com>
2023-10-12 23:01:27 +0900

    refactor: #45 파라미터 명 수정

--- src/main/java/com/dnd/MusicLog/log/dto/GetMyPlaylistDto.java
+++ src/main/java/com/dnd/MusicLog/log/dto/GetMyPlaylistDto.java
@@ -3,5 +3,5 @@ package com.dnd.MusicLog.log.dto;
 import java.util.List;
 
 public record GetMyPlaylistDto(String feelingColor, String timeColor, String weatherColor, String seasonColor,
-                               String date, String text, List<RandomMyPlaylistDto> randomMyPlaylistDtoList) {
+                               String dateRangeText, String filterText, List<RandomMyPlaylistDto> randomMyPlaylistDtoList) {
 }

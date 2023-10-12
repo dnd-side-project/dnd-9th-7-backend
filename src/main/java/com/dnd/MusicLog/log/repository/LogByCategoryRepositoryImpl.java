f795621621e373c1a452b23e5629b668cadcc511
hyeonwook98 <hyeonwook98@naver.com>
2023-10-12 22:57:25 +0900

    feat: #45 마이플레이리스트 결과 조회 메서드 구현

--- src/main/java/com/dnd/MusicLog/log/repository/LogByCategoryRepositoryImpl.java
+++ src/main/java/com/dnd/MusicLog/log/repository/LogByCategoryRepositoryImpl.java
@@ -1,5 +1,6 @@
 package com.dnd.MusicLog.log.repository;
 
+import com.dnd.MusicLog.log.entity.Log;
 import com.dnd.MusicLog.log.enums.Feeling;
 import com.dnd.MusicLog.log.enums.Season;
 import com.dnd.MusicLog.log.enums.Time;
@@ -8,6 +9,10 @@ import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static com.dnd.MusicLog.log.entity.QLog.log;
 
@@ -18,7 +23,7 @@ public class LogByCategoryRepositoryImpl implements LogByCategoryRepository {
     private final JPAQueryFactory jpaQueryFactory;
 
     @Override
-    public Long findRecordCountByCategory(long userId, Feeling feeling, Time time, Weather weather, Season season) {
+    public Long getRecordCountByCategory(long userId, Feeling feeling, Time time, Weather weather, Season season) {
         return jpaQueryFactory
             .select(log.count())
             .from(log)
@@ -29,6 +34,25 @@ public class LogByCategoryRepositoryImpl implements LogByCategoryRepository {
                 weatherEq(weather),
                 seasonEq(season))
             .fetchCount();
+    }
+
+    @Override
+    public List<Log> getMyPlaylistByCategory(long userId, Feeling feeling, Time time, Weather weather, Season season) {
+        List<Log> logList = jpaQueryFactory
+            .selectFrom(log)
+            .leftJoin(log.customMusic).fetchJoin()
+            .leftJoin(log.spotifyMusic).fetchJoin()
+            .leftJoin(log.spotifyMusic.album).fetchJoin()
+            .where(log.user.id.eq(userId),
+                log.temp.eq(false),
+                feelingEq(feeling),
+                timeEq(time),
+                weatherEq(weather),
+                seasonEq(season))
+            .fetch();
+
+        Collections.shuffle(logList);
+        return logList.stream().limit(5).collect(Collectors.toList());
     }
 
     private BooleanExpression feelingEq(Feeling feeling) {

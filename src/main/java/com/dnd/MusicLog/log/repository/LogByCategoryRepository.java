7f31cabbe3370acad7331f766dc7b699cb851485
hyeonwook98 <hyeonwook98@naver.com>
2023-10-12 22:57:13 +0900

    feat: #45 마이플레이리스트 결과조회 메서드 선언

--- src/main/java/com/dnd/MusicLog/log/repository/LogByCategoryRepository.java
+++ src/main/java/com/dnd/MusicLog/log/repository/LogByCategoryRepository.java
@@ -1,12 +1,18 @@
 package com.dnd.MusicLog.log.repository;
 
+import com.dnd.MusicLog.log.entity.Log;
 import com.dnd.MusicLog.log.enums.Feeling;
 import com.dnd.MusicLog.log.enums.Season;
 import com.dnd.MusicLog.log.enums.Time;
 import com.dnd.MusicLog.log.enums.Weather;
 
+import java.util.List;
+
+
 public interface LogByCategoryRepository {
 
-    Long findRecordCountByCategory(long userId, Feeling feeling, Time time, Weather weather, Season season);
+    Long getRecordCountByCategory(long userId, Feeling feeling, Time time, Weather weather, Season season);
+
+    List<Log> getMyPlaylistByCategory(long userId, Feeling feeling, Time time, Weather weather, Season season);
 
 }

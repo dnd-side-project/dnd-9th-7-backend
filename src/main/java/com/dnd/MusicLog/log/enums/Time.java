3630c9c620b0ed4fcf25627c34924cd333cfceb8
hyeonwook98 <hyeonwook98@naver.com>
2023-10-12 22:56:07 +0900

    feat: #45 카테고리 인터페이스 적용 및 상태명 추가

--- src/main/java/com/dnd/MusicLog/log/enums/Time.java
+++ src/main/java/com/dnd/MusicLog/log/enums/Time.java
@@ -7,12 +7,13 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
-public enum Time {
+public enum Time implements Category{
 
-    MORNING("E9FFAC"),
-    LUNCH("CAED66"),
-    DINNER("1F489A"),
-    DAWN("3D33AA");
+    MORNING("아침", "E9FFAC"),
+    LUNCH("점심", "CAED66"),
+    DINNER("저녁", "1F489A"),
+    DAWN("새벽", "3D33AA");
 
+    private String status;
     private String color;
 }
